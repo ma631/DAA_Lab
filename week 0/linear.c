4d4273f882eeee88d6deda70fac7b50c8005a5c3
ma631 <73124430+ma631@users.noreply.github.com>
2021-08-19 01:39:31 +0530

    Update linear.c

--- week 0/linear.c
+++ week 0/linear.c
@@ -1,1 +1,28 @@
-
+#include <stdio.h>
+int find_element(int a[],int n,int key){
+    int i;
+    for(i=0;i<n;i++){
+        if(a[i]==key){
+            break;
+        }
+    }
+    if(i<n){
+        printf("element found at %d index",i);
+    }
+    else {
+        printf("element not found");
+    }
+}
+int main() {
+    int t,n,a[n],key;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d",&n);
+        for(int i=0;i<n;i++){
+            scanf("%d",&a[i]);
+        }
+        printf("enter the element to be found: ");
+        scanf("%d",&key);
+        find_element(a,n,key);
+    }
+}
